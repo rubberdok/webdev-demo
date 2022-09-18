d6c81766b3c728ede25d5474da307be01f6a7b8f
hermannm <hermann.morkrid@gmail.com>
2022-09-18 14:55:12 +0200

    configure apollo client for django-demo frontend

--- django-demo/frontend/src/App.tsx
+++ django-demo/frontend/src/App.tsx
@@ -1,5 +1,15 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { TodoPage } from "./TodoPage";
 
+const graphqlClient = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+  cache: new InMemoryCache(),
+});
+
 export const App: React.FC = () => {
-  return <TodoPage />;
+  return (
+    <ApolloProvider client={graphqlClient}>
+      <TodoPage />
+    </ApolloProvider>
+  );
 };
